10d107e445a7c7422d735d530232c2363690de0d
Grigoriy Levanov <gregory@levanov.io>
2023-11-25 14:57:33 +0100

    add current hour handling

--- src/mapper.js
+++ src/mapper.js
@@ -1,11 +1,17 @@
+const HOURS_OFFSET = 2
+
 export const mapData = (data) => {
-  const prices = data.map(({ price }) => price)
+  const hour = new Date().getHours()
+  const hourIndex = data.findIndex(item => item.hour === hour)
+  const slice = data.slice(hourIndex > HOURS_OFFSET ? hourIndex - HOURS_OFFSET : 0)
+
+  const prices = slice.map(({ price }) => price)
 
   const maxValue = Math.max(...prices)
   const limit = 90
   const scaling = limit / maxValue
 
-  return data.map(item => ({
+  return slice.map(item => ({
     ...item,
     height: Math.round(item.price * scaling)
   }))
--- src/ui/chart.js
+++ src/ui/chart.js
@@ -1,35 +1,43 @@
 export const Chart = (data) => {
-    const chartNode = document.createElement('div')
-    chartNode.classList.add('chart')
+  const chartNode = document.createElement('div')
+  chartNode.classList.add('chart')
 
-    const headerNode = document.createElement('div')
-    headerNode.classList.add('header')
-    headerNode.appendChild(document.createTextNode('Электричество в Мальмё'))
-    const subHeaderNode = document.createElement('span')
-    subHeaderNode.classList.add('subHeader')
-    subHeaderNode.appendChild(document.createTextNode('эре/час'))
-    headerNode.appendChild(subHeaderNode)
-    chartNode.appendChild(headerNode)
+  const headerNode = document.createElement('div')
+  headerNode.classList.add('header')
+  headerNode.appendChild(document.createTextNode('Электричество в Мальмё'))
+  const subHeaderNode = document.createElement('span')
+  subHeaderNode.classList.add('subHeader')
+  subHeaderNode.appendChild(document.createTextNode('эре/час'))
+  headerNode.appendChild(subHeaderNode)
+  chartNode.appendChild(headerNode)
 
-    const graphNode = document.createElement('div')
-    graphNode.classList.add('data')
-    data.forEach(({ hour, price, height }) => {
-        const barNode = document.createElement('div')
-        barNode.classList.add('percentage')
+  const graphNode = document.createElement('div')
+  graphNode.classList.add('data')
 
-        const priceNode = document.createElement('span')
-        priceNode.classList.add('price')
-        priceNode.setAttribute('style', `--bar-height: ${height}%`)
-        priceNode.appendChild(document.createTextNode(Math.floor(price)))
-        
-        const hourNode = document.createElement('span')
-        hourNode.appendChild(document.createTextNode(hour))
-        barNode.appendChild(priceNode)
-        barNode.appendChild(hourNode)
-        
-        graphNode.appendChild(barNode)
-    })
-    chartNode.appendChild(graphNode)
-    
-    return chartNode
+  const currentHour = new Date().getHours()
+  let isCurrentHourFound = false
+
+  data.forEach(({ hour, price, height }) => {
+    const barNode = document.createElement('div')
+    barNode.classList.add('percentage')
+
+    const priceNode = document.createElement('span')
+    priceNode.classList.add('price')
+    if (!isCurrentHourFound && hour === currentHour) {
+      priceNode.classList.add('current')
+      isCurrentHourFound = true
+    }
+    priceNode.setAttribute('style', `--bar-height: ${height}%`)
+    priceNode.appendChild(document.createTextNode(Math.floor(price)))
+
+    const hourNode = document.createElement('span')
+    hourNode.appendChild(document.createTextNode(hour))
+    barNode.appendChild(priceNode)
+    barNode.appendChild(hourNode)
+
+    graphNode.appendChild(barNode)
+  })
+  chartNode.appendChild(graphNode)
+
+  return chartNode
 }
