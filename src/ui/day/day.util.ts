341fd28a69f3606913a92d416ae46f7d5f951ea5
Grigoriy Levanov <gregory@levanov.io>
2024-03-08 13:24:44 +0100

    convert day util

--- src/ui/day/day.util.ts
+++ src/ui/day/day.util.ts
@@ -1,17 +1,17 @@
 import { isToday, isTomorrow } from "date-fns";
 
-const capitalizeFirstLetter = (str) =>
+const capitalizeFirstLetter = (str: string) =>
   str.charAt(0).toUpperCase() + str.slice(1);
 
-const formatDay = (start) =>
+const formatDay = (start: Date) =>
   start.toLocaleDateString("ru-RU", { weekday: "long" });
 
 const DAY_NAME = {
   TODAY: "Сегодня",
   TOMORROW: "Завтра",
-};
+} as const;
 
-export const getDayName = (start) => {
+export const getDayName = (start: Date) => {
   if (isToday(start)) {
     return DAY_NAME.TODAY;
   }
@@ -20,5 +20,5 @@ export const getDayName = (start) => {
     return DAY_NAME.TOMORROW;
   }
 
-  return capitalizeFirstLetter(formatDay());
+  return capitalizeFirstLetter(formatDay(start));
 };
